ed869f2abb3b911e9c3ff8e284af43605cc9bcec
Atrijo Gangopadhyay <55339248+atrijo2001@users.noreply.github.com>
2021-09-22 12:39:15 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (79.93%) - LeetHub

--- valid-parentheses/valid-parentheses.cpp
+++ valid-parentheses/valid-parentheses.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
+    bool isMatching(char a, char b){
+        return ((a=='(' and b == ')') or (a=='{' and b=='}') or (a=='[' and b==']'));
+    }
     bool isValid(string s) {
-        stack<char> st;
-        char c;
-        for(int i = 0; i<s.length(); i++){
-            c = s.at(i);
-            if(st.empty()){
-                st.push(s[i]);
-            } else if (st.top() == '(' && c == ')' || st.top() == '{' && c == '}' || st.top() == '[' && c == ']')
-        {
-            st.pop();
-        }else{
-                st.push(c);
+        stack<char>st;
+        for(char x: s){
+            if(x=='(' or x=='{' or x=='['){
+                st.push(x);
+            } else{
+                if(st.empty()==true){
+                    return false;
+                } else if(isMatching(st.top(), x)==false){
+                    return false;
+                } else{
+                    st.pop();
+                }
             }
         }
-        if(st.empty()){
-            return true;
-        }
-        return false;
+        return st.empty()==true;
     }
 };
