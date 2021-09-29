e1ef3798d94cb035d58d0b36f553d2509e8be968
Atrijo Gangopadhyay <55339248+atrijo2001@users.noreply.github.com>
2021-09-30 02:30:24 +0530

    Time: 636 ms (5.00%), Space: 329.1 MB (5.01%) - LeetHub

---
+++ combination-sum/combination-sum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>>ans;
+    void helper(vector<int> v, int idx, int target, vector<int>&ds){
+        if(idx==v.size()){
+            if(target==0){
+                ans.push_back(ds);
+            }
+            return;
+        }
+        if(target>0){
+            ds.push_back(v[idx]);
+            helper(v, idx, target-v[idx], ds);
+            
+            ds.pop_back();
+        }
+        helper(v, idx+1, target, ds);
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int> ds;
+        helper(candidates, 0, target, ds);
+        return ans;
+    }
+};
