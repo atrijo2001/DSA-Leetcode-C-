d2eb05d53ef524040c862716d58fef8a721c5104
Atrijo Gangopadhyay <55339248+atrijo2001@users.noreply.github.com>
2021-11-06 10:43:53 +0530

    Time: 12 ms (42.79%), Space: 10.9 MB (14.36%) - LeetHub

---
+++ single-number-iii/single-number-iii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        unordered_map<int, int> mp;
+        vector<int> ans;
+        for(int i=0; i<nums.size(); i++){
+            mp[nums[i]]++;
+        }
+        for(auto x: mp){
+            if(x.second==1) ans.push_back(x.first);
+        }
+        return ans;
+    }
+};
