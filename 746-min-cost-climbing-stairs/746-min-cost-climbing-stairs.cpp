926d35a02c88c8d3e5deb5e0b848934852c36394
Atrijo Gangopadhyay <55339248+atrijo2001@users.noreply.github.com>
2022-01-17 20:15:03 +0530

    Time: 12 ms (24.04%), Space: 13.9 MB (26.02%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int helper(vector<int> &costs, int ind, vector<int> &dp){
         if(ind<0) return 0;
-        if(ind == 1 or ind==1) return costs[ind];
+        if(ind == 0 or ind==1) return costs[ind];
         if(dp[ind]!=-1) return dp[ind];
         int left = helper(costs, ind-1, dp) + costs[ind];
         int right = helper(costs, ind-2, dp) +  costs[ind];
