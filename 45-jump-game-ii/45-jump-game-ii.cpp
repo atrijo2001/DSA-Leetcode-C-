f60e40deee255d6a4236002a6bbd7de1987bd61a
Atrijo Gangopadhyay <55339248+atrijo2001@users.noreply.github.com>
2022-01-24 16:51:55 +0530

    Time: 1281 ms (5.01%), Space: 18.3 MB (5.47%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -9,15 +9,6 @@ public:
         }
         return dp[ind];
     }
-    int help(vector<int>& nums,  vector<int>& dp, int start){
-        if(start >= size(nums)-1) return 0;
-        if(dp[start] != 99999) return dp[start];
-        
-        for(int i=1; i<=nums[start]; i++){
-            dp[start] = min(dp[start], 1+help(nums, dp, start+i));
-        }
-        return dp[start];
-    }
     int jump(vector<int>& nums) {
         vector<int> dp(nums.size(), 99999);
         int ans = helper(0,nums,nums.size(), dp);
