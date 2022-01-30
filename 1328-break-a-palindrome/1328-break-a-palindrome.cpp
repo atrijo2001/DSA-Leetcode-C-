97f7df116593cf85c8cb7a3181c5882db9a58b19
Atrijo Gangopadhyay <55339248+atrijo2001@users.noreply.github.com>
2022-01-30 23:26:56 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (45.99%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string breakPalindrome(string s) {
+        int n = s.length();
+        if(n==1) return "";
+        for(int i=0; i<n; i++){
+            int j = n-1-i;
+            if(i==j) continue;
+            if(s[i]!='a'){
+                s[i]='a';
+                return s;
+            }
+            j--;
+        }
+        s[n-1] = 'b';
+        return s;
+    }
+};
