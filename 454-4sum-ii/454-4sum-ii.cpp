67c3d1be38d886066431f98931632d95194e4002
Atrijo Gangopadhyay <55339248+atrijo2001@users.noreply.github.com>
2022-02-03 13:01:49 +0530

    Time: 411 ms (26.79%), Space: 24.3 MB (94.70%) - LeetHub

--- 454-4sum-ii/454-4sum-ii.cpp
+++ 454-4sum-ii/454-4sum-ii.cpp
@@ -11,7 +11,8 @@ public:
         for(int i=0; i<nums3.size(); i++){
             for(int j=0; j<nums4.size(); j++){
                 int sum = nums3[i]+nums4[j];
-               if(mp.count(0-sum)) ans+=mp[0-sum];
+               // if(mp.count(0-sum)) ans+=mp[0-sum];
+                if(mp.find(0-sum)!=mp.end()) ans+=mp[0-sum];
             }
         }
         return ans;
